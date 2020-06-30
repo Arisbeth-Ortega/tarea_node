de3ef4f4581edc7f87a5394b18cfa0eeff6260ab
Arisbeth Ortega <arisbeth.areli@gmail.com>
2020-06-30 18:10:26 -0500

    Cambio

--- tarea.js
+++ tarea.js
@@ -1,15 +1,18 @@
 const express= require ('express')
 const servidor= express();
+var num=0;
+function peticion(req,res)
+{
+    num= contador +1;
+    
 
+}
 servidor.get('/',function (req, res) {
-    for(let i=0; i<i ;i++){
-        res.send('Bienvenido visitante'+i);
 
-    }
-   
+          res.send('Bienvenido visitante'+''+ num++);
 
 });
-
+ 
 servidor.listen(3000,function(){
     console.log('Estoy escuchando 3000');
 
